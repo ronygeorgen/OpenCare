933d673a52b182f799ca8d15b7537b6268cfd1c3
Rojin4455 <rojinsaji35@gmail.com>
2025-05-16 00:25:50 +0530

    replace sqlite3 postgres

--- OpenCare/settings.py
+++ OpenCare/settings.py
@@ -159,10 +159,21 @@ WSGI_APPLICATION = 'OpenCare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("NAME"),
+        'USER': "postgres",
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': '5432',
     }
 }
 
